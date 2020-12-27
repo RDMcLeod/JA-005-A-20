d8899389856a7e00bba03c83e03ea59b7affb731
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-26 20:13:18 -0500

    Basic D123

--- Javascript-002/js/rdm.js
+++ Javascript-002/js/rdm.js
@@ -1,5 +1,5 @@
-document,
-  (getElementById("button").onclick = function () {
-    document.getElementById("confirm").innerHTML =
-      "Order placed. Check email for confirmation";
-  });
+document.getElementById("button").onclick = function () {
+  document.getElementById("confirm").innerHTML =
+    "Order placed. Check email for confirmation";
+  document.getElementById("bitton").style.display = "none";
+};
