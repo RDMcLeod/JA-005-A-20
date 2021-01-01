6da4dc4ecbb623061faae6855f2d4e6ab190e944
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-01-01 00:10:29 -0500

    CF Webdev P5 Final

--- Javascript-002-P1/js/app.js
+++ Javascript-002-P1/js/app.js
@@ -1,1 +1,22 @@
-console.log("hello from javascript -Updated")
+const linkCategory = document.querySelector("#linkCategory");
+
+let linkCategories = [];
+
+linkCategory.addEventListener("keydown", function (event) {
+  
+  
+  if (event.keycode === 188) {
+      event.preventDefault();
+    
+      linkCategories.push(linkCategory.value);
+
+      linkCategory.value='';
+
+    //Display the updated catagories
+      displayLinkCategories(); 
+    
+  }
+})
+function displayLinkCategories() {
+  console.log("display Link Categories");
+}
