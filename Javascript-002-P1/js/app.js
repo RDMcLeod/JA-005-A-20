ba065d993145a5353754d19eda0c5b1cbe59b5a7
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-01-02 05:41:35 -0500

    CF Webdev p6 ES6 Arrow Func-this

--- Javascript-002-P1/js/app.js
+++ Javascript-002-P1/js/app.js
@@ -1,6 +1,11 @@
 const linkCategory = document.querySelector("#linkCategory");
 
+const submitButton = document.querySelector("#submitButton");
+
+
+
 let linkCategories = [];
+let links =[];
 
 linkCategory.addEventListener("keydown", function (event) {
   
@@ -19,4 +24,37 @@ linkCategory.addEventListener("keydown", function (event) {
 })
 function displayLinkCategories() {
   console.log("display Link Categories");
+  
 }
+
+console.log(this);
+
+submitButton.addEventListener('click', (event) =>{
+
+//stop form from submitting
+   event.preventDefault(); 
+
+
+  const title = linkTitle.value; 
+  const url = linkUrl.value; 
+  const categories = linkCategories;
+  
+  
+  const newLink = {
+   title,
+   url,
+   categories
+
+  } 
+    // push new link to array
+    links.push(newLink);
+
+    linkTitle.value='';
+    linkUrl.value='';
+    linkCategory.value='';
+    linkCategories=[];
+
+    displayLinkCategories();
+   
+    
+});
