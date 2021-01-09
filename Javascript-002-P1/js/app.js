0c9ef665ef9baccf09c9586cb8c9fffa8055bba4
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-01-09 13:07:08 -0500

    CF Webdev p14 applying animations

--- Javascript-002-P1/js/app.js
+++ Javascript-002-P1/js/app.js
@@ -5,129 +5,120 @@ const cancelButton = document.querySelector("#cancelButton");
 const addLinkPanel = document.querySelector("#addLinkPanel");
 const addedCategories = document.querySelector("#addedCategories");
 
-const addLinkContainer =document.querySelector("#addLinkContainer");
+const addLinkContainer = document.querySelector("#addLinkContainer");
 
 let editIndex = -1;
 
 let linkCategories = [];
 let links = [
-	{
-		title: 'New Link 1',
-		url: 'url1.com',
-		categories: ['node', 'angular']
-	},
-	{
-		title: 'New Link 2',
-		url: 'url2.com',
-		categories: ['js', 'angular']
-	},
-	{
-		title: 'New Link 3',
-		url: 'url3.com',
-		categories: ['node', 'bootstrap']
-	}
+  {
+    title: "New Link 1",
+    url: "url1.com",
+    categories: ["node", "angular"],
+  },
+  {
+    title: "New Link 2",
+    url: "url2.com",
+    categories: ["js", "angular"],
+  },
+  {
+    title: "New Link 3",
+    url: "url3.com",
+    categories: ["node", "bootstrap"],
+  },
 ];
 
 displayLinks();
 
-addBtn.addEventListener('click', (event) => {
-	console.log("Add btn clicked");
-	showFormPanel();
+addBtn.addEventListener("click", (event) => {
+  console.log("Add btn clicked");
+  showFormPanel();
 });
 
-cancelButton.addEventListener('click', (event) => {
-	event.preventDefault();
-	console.log("Cancel button clicked");
+cancelButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("Cancel button clicked");
 
-	hideFormPanel();
+  hideFormPanel();
 });
 
 function showFormPanel() {
-	addLinkContainer.classList.remove('hidden');
-	displayLinkCategories();
+  addLinkContainer.classList.remove("hidden");
+  displayLinkCategories();
 }
 
 function hideFormPanel() {
-	addLinkContainer.classList.add('hidden');
-	clearLinkForm();
-
+  addLinkContainer.classList.add("hidden");
+  clearLinkForm();
 }
 
-linkCategory.addEventListener('keydown', function (event) {
+linkCategory.addEventListener("keydown", function (event) {
+  if (event.keyCode === 188) {
+    event.preventDefault();
 
-	if (event.keyCode === 188) {
-		event.preventDefault();
+    linkCategories.push(linkCategory.value);
 
-		linkCategories.push(linkCategory.value);
+    linkCategory.value = "";
 
-		linkCategory.value = '';
-
-		//Display the updated categories
-		displayLinkCategories();
-	}
-})
+    //Display the updated categories
+    displayLinkCategories();
+  }
+});
 
 function displayLinkCategories() {
-	console.log("Displaying Link Categories");
-	addedCategories.innerHTML = '';
-	for (let category of linkCategories) {
-		var categoryHTMLString = `<span class="category">${category}</span>`;
-		addedCategories.innerHTML += categoryHTMLString;
-	}
+  console.log("Displaying Link Categories");
+  addedCategories.innerHTML = "";
+  for (let category of linkCategories) {
+    var categoryHTMLString = `<span class="category">${category}</span>`;
+    addedCategories.innerHTML += categoryHTMLString;
+  }
 }
 
 function clearLinkForm() {
-	linkTitle.value = '';
-	linkUrl.value = '';
-	linkCategory.value = '';
-	linkCategories = [];
-	addedCategories.innerHTML = '';
+  linkTitle.value = "";
+  linkUrl.value = "";
+  linkCategory.value = "";
+  linkCategories = [];
+  addedCategories.innerHTML = "";
 }
 
+submitButton.addEventListener("click", (event) => {
+  //Stop form from submitting
+  event.preventDefault();
 
+  const title = linkTitle.value;
+  const url = linkUrl.value;
+  const categories = linkCategories;
 
-submitButton.addEventListener('click', (event) => {
-	//Stop form from submitting
-	event.preventDefault();
+  const newLink = {
+    title,
+    url,
+    categories,
+  };
+  if (editIndex === -1) {
+    //push new link to array
+    links.unshift(newLink);
+  } else {
+    links[editIndex] = newLink;
+    editIndex = -1;
+  }
 
-	const title = linkTitle.value;
-	const url = linkUrl.value;
-	const categories = linkCategories;
+  clearLinkForm();
 
-	const newLink = {
-		title,
-		url,
-		categories
-	}
-	if (editIndex === -1) {
-		//push new link to array
-		links.unshift(newLink);
-	}
-	else {
-		links[editIndex] = newLink;
-		editIndex = -1;
-	}
+  displayLinkCategories();
 
-	clearLinkForm();
+  //hide the addLinkPanel form
+  hideFormPanel();
 
-	displayLinkCategories();
-
-	//hide the addLinkPanel form
-	hideFormPanel();
-
-	displayLinks();
-
-
+  displayLinks();
 });
 
-
 function displayLinks() {
-	linksList.innerHTML = '';
+  linksList.innerHTML = "";
 
-	let index = 0;
-	for (let link of links) {
-
-		let linkHTMLString = `
+  let index = 0;
+  for (let link of links) {
+    let linkHTMLString = `
 		<div class="flex-item">
 			<div class="link panel">
 				<div class="link-options">
@@ -140,36 +131,34 @@ function displayLinks() {
 				<p class="link-date">${Date.now()}</p>
 				<div class="categories">
 					Categories:`;
-		for (let category of link.categories) {
-			linkHTMLString += `<span class="category">${category}</span>`;
-		}
+    for (let category of link.categories) {
+      linkHTMLString += `<span class="category">${category}</span>`;
+    }
 
-		linkHTMLString += `
+    linkHTMLString += `
 					</div>
 				</div>	
 			</div>	
-			`
-			;
+			`;
 
-		linksList.innerHTML += linkHTMLString;
-		index++;
-	}
+    linksList.innerHTML += linkHTMLString;
+    index++;
+  }
 }
 
 function deleteLink(index) {
-	console.log("Deleting link at index", index);
-	links.splice(index, 1);
-	displayLinks();
+  console.log("Deleting link at index", index);
+  links.splice(index, 1);
+  displayLinks();
 }
 
 function editLink(index) {
-	console.log("Editing link at index", index);
+  console.log("Editing link at index", index);
 
-	editIndex = index;
-	linkTitle.value = links[index].title;
-	linkUrl.value = links[index].url;
-	linkCategories = links[index].categories;
+  editIndex = index;
+  linkTitle.value = links[index].title;
+  linkUrl.value = links[index].url;
+  linkCategories = links[index].categories;
 
-	showFormPanel();
-
+  showFormPanel();
 }
