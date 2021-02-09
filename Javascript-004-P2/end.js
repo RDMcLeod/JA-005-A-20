c0b6746cecb58173552e24b77342180b0d8f46e5
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-02-09 17:18:11 -0500

    Update style end page-game-js

--- Javascript-004-P2/end.js
+++ Javascript-004-P2/end.js
@@ -1,15 +1,14 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-
-// testing with console.log
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-  console.log(username.value);
-
   saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
-  console.log("cliekced the save button!");
+  console.log("clicked the save button!");
   e.preventDefault();
 };
