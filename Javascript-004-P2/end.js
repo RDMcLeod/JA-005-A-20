5391c93438c833e52f03cc068b10f2d86bd2e7ae
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-02-15 16:21:29 -0500

    Update endjs

--- Javascript-004-P2/end.js
+++ Javascript-004-P2/end.js
@@ -25,7 +25,7 @@ saveHighScore = (e) => {
   highScores.sort( (a,b) ==> b.score - a.score)
   highScores.splice(5);
 
-  localStorage.setItem('highScores' JSON.stringify(highScores));
+  localStorage.setItem('highScores', JSON.stringify(highScores));
   window.location.assign('/');
 
   
