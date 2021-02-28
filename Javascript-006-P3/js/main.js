a56207ad9309179bb02e8b527ac33a032e3e6bd2
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-02-27 19:21:47 -0500

    Update main.js file

--- Javascript-006-P3/js/main.js
+++ Javascript-006-P3/js/main.js
@@ -1,1 +1,19 @@
-document.addEventListener("readyStateChange", (event) => {});
+document.addEventListener("readyStateChange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+const initApp = () => {
+  //set the focus
+
+  //3 listeners clear text
+  const form = document.getElementById("searchBar");
+  form.addEventListener("submit", submitTheSearch);
+};
+// Procedural workflow function
+const submitTheSearch = (event) => {
+  event.preventDefault();
+  //delete seacrh results
+  //Process the search
+  //set the focus
+};
