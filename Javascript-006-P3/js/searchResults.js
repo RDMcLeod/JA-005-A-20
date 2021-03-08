87f800022363e024019564a8642feb8ea37806ab
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-03-08 18:01:49 -0500

    Update sr-jd file

--- Javascript-006-P3/js/searchResults.js
+++ Javascript-006-P3/js/searchResults.js
@@ -20,4 +20,19 @@ const createResultItem = (result) => {
   const resultTitle = document.createElement("div");
   resultTitle.classList.add("resultTitle");
   const link = document.createElement("a");
+  link.href = `https://enwikipedia.org/?curid=${result.id}`;
+  link.textContent = result.title;
+  link.target = "_blank";
+  resultTitle.append(link);
+  resultItem.append(resultTitle);
+  return resultItem;
+};
+const createResultImage = (result) => {
+  const resultImage = document.createElement("div");
+  resultImage.classList.add("resultImage");
+  const img = document.createElement("img");
+  img.src = result.img;
+  img.alt = result.title;
+  resultImage.append(img);
+  return resultImage;
 };
