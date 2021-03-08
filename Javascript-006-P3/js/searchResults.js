997092d01d5e03f90c1c04239228b49c673501a6
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-03-07 19:52:03 -0500

    Update searchResults.js file

---
+++ Javascript-006-P3/js/searchResults.js
@@ -0,0 +1,16 @@
+export const buildSearchResults = (resultArray) => {
+  resultArray.forEach((result) => {
+    const resultItem = creatResultItem(result);
+    const resultContents = document.createElement("div");
+    resultContents.classList.add("resultContents");
+    if (result.img) {
+      const resultImage = createResultImage(result);
+      resultContents.append(resultImage);
+    }
+    const resultText = createResultText(result);
+    resultContents.append(resultText);
+    resultItem.append(resultContents);
+    const searchResults = document.getElementById("searchResults");
+    searchResults.append(resultItem);
+  });
+};
