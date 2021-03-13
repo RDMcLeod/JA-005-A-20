5d688a2d26e78cf91fab9256cdcaf0784a33e140
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-03-13 16:16:35 -0500

    Update sb-js file

--- Javascript-006-P3/js/searchBar.js
+++ Javascript-006-P3/js/searchBar.js
@@ -2,14 +2,23 @@ export const setSearchFocus = () => {
   document.getElementById("search").focus();
 };
 
-export const showClearTextButton = ()=> {
-  const search =document.getElementById("search");
-  const clear= document,getElementById("clear");
+export const showClearTextButton = () => {
+  const search = document.getElementById("search");
+  const clear = document.getElementById("clear");
   if (search.nodeValue.length) {
-    clear.classlist.remove("none")
+    clear.classlist.remove("none");
     clear.classlist.add("flex");
   } else {
     clear.classlist.add("none");
     clear.classlist.remove("flex");
   }
+};
+
+export const clearSearchText = (event) => {
+  event.preventDefault();
+  document.getElementById("search").value = "";
+  const clear = document.getElementById("clear");
+  clear.classList.add("none");
+  clear.classList.remove("flex");
+  setSearchFocus();
 };
