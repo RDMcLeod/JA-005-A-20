ed1114d6cf06d183ead9f126e63f516e3e83b191
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-03-20 16:54:02 -0400

    Update mail-js

--- Javascript-010-P5/js/main.js
+++ Javascript-010-P5/js/main.js
@@ -2,3 +2,25 @@ import ToDoList from "./todolist.js";
 import ToDoItem from "./todoitem.js";
 
 const toDoList = new ToDoList();
+
+//Launch app
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readystate === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  //Add listeners
+
+  // procedural
+  //Load list object
+  //refresh the page
+  refreshThePage();
+};
+const refreshThePage = () => {
+  clearListPage();
+  //renderList
+  //clearItemEntryField();
+  //setFocusOnItemEntryField();
+};
