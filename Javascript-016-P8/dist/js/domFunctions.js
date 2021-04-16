375508167af69953e29dbbd16996c96873575315
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-04-16 17:31:55 -0400

    Updatedf.js

--- Javascript-016-P8/dist/js/domFunctions.js
+++ Javascript-016-P8/dist/js/domFunctions.js
@@ -2,3 +2,8 @@ export const addSpinner = (element) => {
   animateButton(element);
   setTimeout(animateButton, 1000, element);
 };
+const animateButton = (element) => {
+  element.classList.toggle("none");
+  element.nextElementSibling.classList.toggle("block");
+  element.nextElementSibling.classList.toggle("none");
+};
