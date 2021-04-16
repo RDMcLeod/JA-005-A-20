3ee0d37fd2f63c463f67b399b64099914ab09832
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-04-15 22:05:47 -0400

    Update js files

--- Javascript-016-P8/dist/js/CurrentLocation.js
+++ Javascript-016-P8/dist/js/CurrentLocation.js
@@ -6,4 +6,34 @@ export default class CurrentLocation {
     this._lat = null;
     this._unit = "imperial";
   }
+  getName() {
+    return this._name;
+  }
+
+  setName(name) {
+    this._name = name;
+  }
+
+  getLat() {
+    return this._lat;
+  }
+
+  setLat(lat) {
+    this._lat = lat;
+  }
+  getLon() {
+    return this._lon;
+  }
+  SetLon(lon) {
+    this._lon = lon;
+  }
+  getUnit() {
+    return this._unit;
+  }
+  setUnit(unit) {
+    this._unit = unit;
+  }
+  toggleUnit() {
+    this._unit = this._unit === "imperial" ? "metric" : "imperial";
+  }
 }
