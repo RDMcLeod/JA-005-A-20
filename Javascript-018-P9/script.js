6d613d6206ec03c132fe09d5e42bef7aed1f7436
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-05-05 20:54:27 -0400

    Update script.js

--- Javascript-018-P9/script.js
+++ Javascript-018-P9/script.js
@@ -23,6 +23,14 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
 }
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+}
+
+function pauseSong() {}
+
 //event listeners
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
