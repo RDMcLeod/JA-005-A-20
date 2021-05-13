7b015bfafc12718ac63b54e37ac4d546187be30c
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-05-13 18:06:52 -0400

    Update script.js

--- Javascript-020-P10/script.js
+++ Javascript-020-P10/script.js
@@ -1,22 +1,25 @@
 const slider = document.querySelector(".slider-container"),
   slides = Array.from(document.querySelectorAll(".slide"));
 
-let isDraging = false,
+let isDragging = false,
   startPos = 0,
   curentTranslate = 0,
   prevTranslate = 0,
-  animatationID = 0,
-  currentIndex = 0,
+  animationID = 0,
+  currentIndex = 0;
 
 slides.forEach((slide, index) => {
-  const slideImage = slide.querySelector('img')
-  slideImage.addEventListener('dragstart', (e) => e.
-  preventDefault())
+  const slideImage = slide.querySelector("img");
+  slideImage.addEventListener("dragstart", (e) => e.preventDefault());
 
-// Touch Events
-slide.addEventListener('touchStart', touchStart(index))
-slide.addEventListener('touchend', touchStart(index))
-// Mouse Events
-slide.addEventListener('mousedown', touchStart(index))
+  // Touch Events
+  slide.addEventListener("touchStart", touchStart(index));
+  slide.addEventListener("touchend", touchEnd);
+  slide.addEventListener("touchmove", touchEnd);
 
-  })
+  // Mouse Events
+  slide.addEventListener("mousedown", touchStart(index));
+  slide.addEventListener("mouseup", touchEnd);
+  slide.addEventListener("mouseleave", touchEnd);
+  slide.addEventListener("mousemove", touchMove);
+});
