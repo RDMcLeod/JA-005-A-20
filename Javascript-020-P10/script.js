6ba6154743b3ae2084a412c00fcd1606f3fb184d
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-05-13 16:38:18 -0400

    Upate script.js

--- Javascript-020-P10/script.js
+++ Javascript-020-P10/script.js
@@ -1,2 +1,9 @@
 const slider = document.querySelector(".slider-container"),
   slides = Array.from(document.querySelectorAll(".slide"));
+
+let isDraging = false,
+  startPos = 0,
+  curentTranslate = 0,
+  prevTranslate = 0,
+  animatationID = 0,
+  currentIndex = 0,
