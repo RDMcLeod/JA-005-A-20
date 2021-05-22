dde03f8dfa1a6bc1fea9a2d06087075d2c9eb3c4
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-05-21 23:18:08 -0400

    Update ondex & names.js

---
+++ Javascript-022-P12/js/names.js
@@ -0,0 +1,12 @@
+export const namesOne = {
+  0: "Code",
+  1: "Learning",
+  2: "Coding",
+  3: "Program",
+  4: "Coder",
+  5: "Programming",
+  6: "Dev",
+  7: "Webdev",
+  8: "Web",
+  9: "Tech",
+};
