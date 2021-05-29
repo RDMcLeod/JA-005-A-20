c5c83d828010b86d639759b33313dad50aafaa2c
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-05-29 09:10:55 -0400

    Update main.js

--- Javascript-022-P12/js/main.js
+++ Javascript-022-P12/js/main.js
@@ -3,7 +3,7 @@ import { namesOne, namesTwo } from "./names.js";
 const initApp = () => {
   document.getElementById("submitForm").addEventListener("submit", (event) => {
     event.preventDefault();
-    // clear out suggestions
+    clearSuggestions();
     // generate names
     // display names
   });
@@ -15,4 +15,13 @@ const clearSuggestions = () => {
   const display = document.getElementById("suggestionSection");
   if (!display.classList.contains("hidden")) display.classList.toggle("hidden");
   const list = document.querySelector(".suggestionSection ol");
+  list.innerHTML = "";
+  // how to use innerHTML and sanitize the input
+};
+
+const generateNames = () => {
+  const randomNumberArr = [];
+  for (let i = 0; i < 4; ) {
+    const randomNumber = Math.floor(Math.random() * 10);
+  }
 };
