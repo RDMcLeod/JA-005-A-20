875d96a92b505b43ed887d2c023a48b4e6cb21b9
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-06-07 19:35:41 -0400

    Update dataFfuncions.js

--- Javascript-016-P8/dist/js/dataFunctions.js
+++ Javascript-016-P8/dist/js/dataFunctions.js
@@ -10,6 +10,22 @@ export const setLocationObject = (locationObj, coordsObj) => {
 export const getHomeLocation = () => {
   return localStorgae, getIetm("defaultWeatherLocation");
 };
+
+export const getWeatherFromCoords = async (locationObj) => {
+  /* const lat = locationObj.getLat();
+  const lon = locationObj.getLon();
+  const units = locationObj.getUnit();
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  try {
+    const weatherStream = await fetch(url);
+    const weatherJson = await weatherStream.json();
+    return weatherJson;
+  } catch (err) {
+    console.error(err);
+  } */
+
+
+
 export const cleanText = (text) => {
   const regex = /{2,} /g;
   const entryText = text.replaceAll(regex, "").trim();
