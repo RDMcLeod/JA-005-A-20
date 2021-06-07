b32cd2b8ce67e8a5e5f54c0df0bd160658b54660
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-06-07 19:53:07 -0400

    Update rework df.js

--- Javascript-016-P8/dist/js/dataFunctions.js
+++ Javascript-016-P8/dist/js/dataFunctions.js
@@ -1,3 +1,5 @@
+const WEATHER_API_KEY = '';
+
 export const setLocationObject = (locationObj, coordsObj) => {
   const { lat, lon, name, unit } = coordsObj;
   locationObj.setLat(lat);
@@ -11,23 +13,19 @@ export const getHomeLocation = () => {
   return localStorgae, getIetm("defaultWeatherLocation");
 };
 
-export const getWeatherFromCoords = async (locationObj) => {
-  /* const lat = locationObj.getLat();
-  const lon = locationObj.getLon();
-  const units = locationObj.getUnit();
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
-  try {
-    const weatherStream = await fetch(url);
-    const weatherJson = await weatherStream.json();
-    return weatherJson;
-  } catch (err) {
-    console.error(err);
-  } */
+export const getCoordsFromApi = async (entryText, units) => {
+  const regex = /^\d+$/g;
+  const flag = regex.test(entryText) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const encodedUrl = encodeURI(url);
 
+
+export const getWeatherFromCoords = async (locationObj) => {
+  
 
 
 export const cleanText = (text) => {
   const regex = /{2,} /g;
   const entryText = text.replaceAll(regex, "").trim();
   return entryText;
-};
+}
