fdd691acb0d11c6d24a5b27ad33b9c65798c5679
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-07-19 14:34:07 -0400

    Update main.js

--- Javascript-024-P14/dist/js/main.js
+++ Javascript-024-P14/dist/js/main.js
@@ -2,7 +2,6 @@ import GameObj from "./game";
 const Game = new GameObj();
 
 const initApp = () => {
-  // all time data
   initAllTimeData();
   //update scoreboard
   // listen for player choice
@@ -16,4 +15,9 @@ document.addEventListener("DOMContentLoaded", initApp);
 const initAllTimeData = () => {
   Game.setP1AllTime(parseInt(localStorage.getItem("P1AllTime")) || 0);
   Game.setCpAllTime(parseInt(localStorage.getItem("cpAllTime")) || 0);
+};
+const updateScoreBoard = () => {
+  const p1Ats = document.getElementById("p1_all_time_score");
+  p1Ats.textContent = Game.getP1AllTime();
+  p1Ats.ariaLabel = `Player One has ${Game.getP1AllTime()} all time wins.`;
 };
